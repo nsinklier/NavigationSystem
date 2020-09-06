e2432844af2eaa00c9a57d3d588e4dfef91989ae
Nick Sinklier <nicksinklier@gmail.com>
2020-09-06 13:32:52 -0700

    Initial Commit

---
+++ Navigation/Navigation.h
@@ -0,0 +1,19 @@
+//
+//  Navigation.h
+//  Navigation
+//
+//  Created by Nick Sinklier on 9/6/20.
+//  Copyright © 2020 NorpSwizzler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Navigation.
+FOUNDATION_EXPORT double NavigationVersionNumber;
+
+//! Project version string for Navigation.
+FOUNDATION_EXPORT const unsigned char NavigationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Navigation/PublicHeader.h>
+
+
